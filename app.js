b08d2258daaae718d5f2c9b5a0de7e96b2c3428c
caleb <caleb.lopresto30@gmail.com>
2020-07-05 16:57:44 -0400

    Assigns mongoose connection to an external variable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 const router = require('./src/router');
+const Mongo = require('./.mongoKey');
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-mongoose.connect('mongodb+srv://Hgi:Happy1227@cluster0-7qlhj.mongodb.net/quickApiTest?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(Mongo, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.set('view engine', 'ejs');
 
